f9abec092319a092231e0e8ac141dc302ccfcba4
Simangaliso <mncwango.simangaliso@gmail.com>
2022-09-11 05:36:55 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: alway returns 0
+*/
+
+int main(void)
+{
+	char a;
+
+	for (a = 'z' ; a >= 'a' ; a--)
+
+		putchar(a);
+	putchar('\n');
+
+	return (0);
+}
