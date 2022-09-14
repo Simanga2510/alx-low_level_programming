10c0a60974d6ce915b90e2d7568d6fad7e3c8892
Simangaliso <mncwango.simangaliso@gmail.com>
2022-09-14 14:50:11 -0700

    Write a function that computes the absolute value of an intege

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,43 +1,84 @@
-#include "main.h" 
+#include "main.h"                                                                                                                                        
 
-/**
-* print_sign - function to chek a sign of the number 
-* @n: is the int that will use for the argument of the function
-* Return: 0
-*/
+                                                                                                                                                         
 
-int print_sign(int n)
-{
+/**                                                                                                                                                      
 
-	if (n > 0)
+* print_sign - function to chek a sign of the number                                                                                                     
 
-	{
+* @n: is the int that will use for the argument of the function                                                                                          
 
-	_putchar('+');
+* Return: 0                                                                                                                                              
 
-	return (1);
+*/                                                                                                                                                       
 
-}
+                                                                                                                                                         
 
-	else if (n < 0)
+int print_sign(int n)                                                                                                                                    
 
-	{
+{                                                                                                                                                        
 
-	_putchar('-')
+                                                                                                                                                         
 
-	return (-1);
+        if (n > 0)                                                                                                                                       
 
-	}
+                                                                                                                                                         
 
-	else
+        {                                                                                                                                                
 
-	{
+                                                                                                                                                         
 
-	_putchar('0');
+        _putchar('+');                                                                                                                                   
 
-	return (0);
+                                                                                                                                                         
 
-	}
+        return (1);                                                                                                                                      
 
-}
+                                                                                                                                                         
 
+}                                                                                                                                                        
+
+                                                                                                                                                         
+
+        else if (n < 0)                                                                                                                                  
+
+                                                                                                                                                         
+
+        {                                                                                                                                                
+
+                                                                                                                                                         
+
+        _putchar('-')                                                                                                                                    
+
+                                                                                                                                                         
+
+        return (-1);                                                                                                                                     
+
+                                                                                                                                                         
+
+        }                                                                                                                                                
+
+                                                                                                                                                         
+
+        else                                                                                                                                             
+
+                                                                                                                                                         
+
+        {                                                                                                                                                
+
+                                                                                                                                                         
+
+        _putchar('0');                                                                                                                                   
+
+                                                                                                                                                         
+
+        return (0);                                                                                                                                      
+
+                                                                                                                                                         
+
+        }                                                                                                                                                
+
+                                                                                                                                                         
+
+}    
+:wq
